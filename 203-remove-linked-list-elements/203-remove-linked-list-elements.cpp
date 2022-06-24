893f5885028af84b9574d7c68d6336efa7b415c4
Bilal Sajid <bsajid173@gmail.com>
2022-06-24 20:42:59 +0530

    Time: 25 ms (85.77%), Space: 15.3 MB (19.08%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -10,50 +10,50 @@
  */
 class Solution {
 public:
-    ListNode* removeElements(ListNode* head, int val) {
-    if(!head)
-        return head;
-    ListNode* del;
-    while(head != NULL && head->val == val) {
-            del = head;
-            head = head->next;    
-            delete del;
-    }
-      ListNode* temp = head;
-    
-    while(temp && temp->next) {
-        if(temp->next->val == val){
-            del = temp->next;
-            temp->next = temp->next->next;
-            delete del;
-
-        }
-        else
-            temp = temp->next;
-    }
-    return head;
-    
-}
-//     another soln using dummy node
 //     ListNode* removeElements(ListNode* head, int val) {
-        
-//         if(!head)
-//             return head;
-        
-//         ListNode * dummy = new ListNode(0,head);
-//         ListNode* prev = dummy;
-//         ListNode * temp = head;
-//         while(temp){
-//             ListNode* next_node = temp->next;
-//             if(temp->val == val){
-//                 delete temp;
-//                 prev->next = next_node;
-//             }
-//             else{
-//                 prev = temp;
-//             }
-//             temp = next_node;
-//         }
-//         return dummy->next;
+//     if(!head)
+//         return head;
+//     ListNode* del;
+//     while(head != NULL && head->val == val) {
+//             del = head;
+//             head = head->next;    
+//             delete del;
 //     }
+//       ListNode* temp = head;
+    
+//     while(temp && temp->next) {
+//         if(temp->next->val == val){
+//             del = temp->next;
+//             temp->next = temp->next->next;
+//             delete del;
+
+//         }
+//         else
+//             temp = temp->next;
+//     }
+//     return head;
+    
+// }
+//     another soln using dummy node
+    ListNode* removeElements(ListNode* head, int val) {
+        
+        if(!head)
+            return head;
+        
+        ListNode * dummy = new ListNode(0,head);
+        ListNode* prev = dummy;
+        ListNode * temp = head;
+        while(temp){
+            ListNode* next_node = temp->next;
+            if(temp->val == val){
+                delete temp;
+                prev->next = next_node;
+            }
+            else{
+                prev = temp;
+            }
+            temp = next_node;
+        }
+        return dummy->next;
+    }
 };
