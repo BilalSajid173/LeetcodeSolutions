caa03b4feddd8f53b9a12a879b29bc078f366244
Bilal Sajid <bsajid173@gmail.com>
2022-07-17 14:58:37 +0530

    Time: 554 ms (31.51%), Space: 134.6 MB (75.71%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int start = 0;
-        priority_queue<pair<int,int>> max_heap;
-        int i = 0;
-        while(i<k){
-            max_heap.push({nums[i],i});
-            i++;
-        }
-        vector<int> ans;
-        ans.push_back(max_heap.top().first);
-        while(k<nums.size()){
-            start++;
-            while(!max_heap.empty() && max_heap.top().second < start){
-                max_heap.pop();
-            }
-            max_heap.push({nums[k],k});
-            ans.push_back(max_heap.top().first);
-            k++;
-        }
+//         int start = 0;
+//         priority_queue<pair<int,int>> max_heap;
+//         int i = 0;
+//         while(i<k){
+//             max_heap.push({nums[i],i});
+//             i++;
+//         }
+//         vector<int> ans;
+//         ans.push_back(max_heap.top().first);
+//         while(k<nums.size()){
+//             start++;
+//             while(!max_heap.empty() && max_heap.top().second < start){
+//                 max_heap.pop();
+//             }
+//             max_heap.push({nums[k],k});
+//             ans.push_back(max_heap.top().first);
+//             k++;
+//         }
         
-        return ans;
-        
+//         return ans;
+        deque<int> buffer;
+        vector<int> res;
+
+        for(auto i=0; i<nums.size();++i)
+        {
+            while(!buffer.empty() && nums[i]>=nums[buffer.back()]) buffer.pop_back();
+            buffer.push_back(i);
+
+            if(i>=k-1) res.push_back(nums[buffer.front()]);
+            if(buffer.front()<= i-k + 1) buffer.pop_front();
+        }
+        return res;
     }
 };
