24f929614981647525bafc3803b2c4a32bdcf30a
Bilal Sajid <bsajid173@gmail.com>
2022-07-17 15:15:57 +0530

    Time: 658 ms (19.53%), Space: 148.8 MB (28.54%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         int start = 0;
-//         priority_queue<pair<int,int>> max_heap;
-//         int i = 0;
-//         while(i<k){
-//             max_heap.push({nums[i],i});
-//             i++;
-//         }
-//         vector<int> ans;
-//         ans.push_back(max_heap.top().first);
-//         while(k<nums.size()){
-//             start++;
-//             while(!max_heap.empty() && max_heap.top().second < start){
-//                 max_heap.pop();
-//             }
-//             max_heap.push({nums[k],k});
-//             ans.push_back(max_heap.top().first);
-//             k++;
-//         }
-        
-//         return ans;
-        deque<int> buffer;
-        vector<int> res;
+vector<int> result;
+    if (k == 0) return result;
+    priority_queue<pair<int, int>> w;
+    for (int i = 0, n = (int)nums.size(); i < n; i++) {
+        while (!w.empty() && w.top().second <= i-k)
+            w.pop();
+        w.push(make_pair(nums[i],i));
+        if (i >= k-1)
+            result.push_back(w.top().first);
+    }
+    return result;
+//         deque<int> buffer;
+//         vector<int> res;
 
-        for(auto i=0; i<nums.size();++i)
-        {
-            while(!buffer.empty() && nums[i]>=nums[buffer.back()]) buffer.pop_back();
-            buffer.push_back(i);
+//         for(auto i=0; i<nums.size();++i)
+//         {
+//             while(!buffer.empty() && nums[i]>=nums[buffer.back()]) buffer.pop_back();
+//             buffer.push_back(i);
 
-            if(i>=k-1) res.push_back(nums[buffer.front()]);
-            if(buffer.front()<= i-k + 1) buffer.pop_front();
-        }
-        return res;
+//             if(i>=k-1) res.push_back(nums[buffer.front()]);
+//             if(buffer.front()<= i-k + 1) buffer.pop_front();
+//         }
+//         return res;
     }
 };
