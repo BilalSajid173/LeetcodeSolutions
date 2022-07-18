b820bff013ef083e01d070c71c3eee0194b658c9
Bilal Sajid <bsajid173@gmail.com>
2022-07-18 09:49:18 +0530

    Time: 60 ms (93.25%), Space: 14.4 MB (97.31%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -3,15 +3,16 @@ class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         
-        priority_queue<node> pqmax;
+        // priority_queue<node> pqmax;
         priority_queue<node, vector<node>, greater<node>> pqmin;
+        int mx = INT_MIN;
         for(int i = 0;i<nums.size();i++){
-            pqmax.push({nums[i][0],{i,0}});
+            mx = max(mx, nums[i][0]);
             pqmin.push({nums[i][0],{i,0}});
         }
         
         pair<int,int> ans;
-        ans = {pqmin.top().first, pqmax.top().first};
+        ans = {pqmin.top().first, mx};
         
         while(!pqmin.empty()){
             int i = pqmin.top().second.first;
@@ -19,11 +20,11 @@ public:
             if((j+1) < nums[i].size()){
                 pqmin.pop();
                 pqmin.push({nums[i][j+1], {i,j+1}});
-                pqmax.push({nums[i][j+1], {i,j+1}});
-                int diff = pqmax.top().first - pqmin.top().first;
+                mx = max(mx,nums[i][j+1]);
+                int diff = mx - pqmin.top().first;
                 if(diff < (ans.second - ans.first)){
                     ans.first = pqmin.top().first;
-                    ans.second = pqmax.top().first;
+                    ans.second = mx;
                 }
             } else{
                 break;
