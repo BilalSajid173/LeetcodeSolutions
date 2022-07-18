6eaf6f385d15eabe3824a1a1d349ec375d9c21eb
Bilal Sajid <bsajid173@gmail.com>
2022-07-18 09:42:34 +0530

    Time: 91 ms (58.51%), Space: 17.6 MB (31.56%) - LeetHub

---
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    typedef pair<int,pair<int,int>> node;
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        
+        priority_queue<node> pqmax;
+        priority_queue<node, vector<node>, greater<node>> pqmin;
+        for(int i = 0;i<nums.size();i++){
+            pqmax.push({nums[i][0],{i,0}});
+            pqmin.push({nums[i][0],{i,0}});
+        }
+        
+        pair<int,int> ans;
+        ans = {pqmin.top().first, pqmax.top().first};
+        
+        while(!pqmin.empty()){
+            int i = pqmin.top().second.first;
+            int j = pqmin.top().second.second;
+            if((j+1) < nums[i].size()){
+                pqmin.pop();
+                pqmin.push({nums[i][j+1], {i,j+1}});
+                pqmax.push({nums[i][j+1], {i,j+1}});
+                int diff = pqmax.top().first - pqmin.top().first;
+                if(diff < (ans.second - ans.first)){
+                    ans.first = pqmin.top().first;
+                    ans.second = pqmax.top().first;
+                }
+            } else{
+                break;
+            }
+        }
+        vector<int> v;
+        v.push_back(ans.first);
+        v.push_back(ans.second);
+        return v;
+        
+    }
+};
