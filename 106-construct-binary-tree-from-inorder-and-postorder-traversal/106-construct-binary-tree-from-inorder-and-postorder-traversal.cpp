7e68ab59d371be13e1e252776bfe35d5275a2f45
Bilal Sajid <bsajid173@gmail.com>
2022-07-24 10:40:06 +0530

    Time: 209 ms (5.01%), Space: 221.3 MB (5.97%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int postpointer;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        postpointer = postorder.size() - 1;
+        return helper(inorder, postorder, 0, (inorder.size() - 1));
+        
+    }
+    TreeNode* helper(vector<int> inorder, vector<int> postorder,int s, int e){
+        if(s>e){
+            return NULL;
+        }
+        int idx = -1;
+        int i = s;
+        while(i<=e){
+            if(inorder[i] == postorder[postpointer]){
+                idx = i;
+                break;
+            }
+            i++;
+        }
+        TreeNode * root = new TreeNode(postorder[postpointer]);
+        postpointer--;
+        root->right = helper(inorder, postorder, idx + 1, e);
+        root->left = helper(inorder, postorder, s,idx - 1);
+        return root;
+    }
+    
+};
