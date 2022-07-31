1fba861f9129a26aa9de4bfcae5e3faca5ed1004
Bilal Sajid <bsajid173@gmail.com>
2022-07-31 18:24:38 +0530

    Time: 96 ms (5.30%), Space: 27.6 MB (63.70%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -5,12 +5,12 @@ public:
         int s = 0, e = nums.size() - 1;
         while(s<=e){
             int mid = (s+e)/2;
-            if(target > nums[mid]){
-                s = mid + 1;
+            if(target == nums[mid]){
+                return mid;
             } else if(target < nums[mid]){
                 e = mid - 1;
-            } else if(target == nums[mid]){
-                return mid;
+            } else{
+                s = mid + 1;
             }
         }
         return -1;
