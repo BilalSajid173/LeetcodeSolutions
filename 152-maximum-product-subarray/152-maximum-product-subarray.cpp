509d62f1b4fd1a11b161866f5c8ab28bcf6e72b7
Bilal Sajid <bsajid173@gmail.com>
2022-08-08 21:43:10 +0530

    Time: 11 ms (62.74%), Space: 13.7 MB (71.85%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // Return early if input is empty
-        if (nums.empty()) return 0;
-		
-        int best = INT_MIN;
-        // Running products
-        int maxProd = 1;
-        int minProd = 1;
-        
-        for (int n: nums) {
-            if (n < 0) {
-                // Swap max and min
-                swap(maxProd, minProd);
-            }
-            // Reset to current value if smaller or larger than it
-            // (intuitively means that we start considering a new sub-array)
-            maxProd = max(maxProd*n, n);
-            minProd = min(minProd*n, n);
-            // Update the best
-            best = max(best, maxProd);
+        int n = nums.size(), res = nums[0], l = 1, r = 1;
+        for (int i = 0; i < n; i++) {
+            l =  (l) * nums[i];
+            r =  (r) * nums[n - 1 - i];
+            res = max(res, max(l, r));
+            if(l==0)l=1;
+            if(r==0)r=1;
         }
+        return res;
         
-        return best;
+        //M-2
+//         // Return early if input is empty
+//         if (nums.empty()) return 0;
+		
+//         int best = INT_MIN;
+//         // Running products
+//         int maxProd = 1;
+//         int minProd = 1;
+        
+//         for (int n: nums) {
+//             if (n < 0) {
+//                 // Swap max and min
+//                 swap(maxProd, minProd);
+//             }
+//             // Reset to current value if smaller or larger than it
+//             // (intuitively means that we start considering a new sub-array)
+//             maxProd = max(maxProd*n, n);
+//             minProd = min(minProd*n, n);
+//             // Update the best
+//             best = max(best, maxProd);
+//         }
+        
+//         return best;
     }
 };
