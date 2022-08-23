69591ab13cc5b2b532c0f69b69f107a6ac9b2acc
Bilal Sajid <bsajid173@gmail.com>
2022-08-23 21:47:49 +0530

    Time: 3 ms (58.67%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 1) return true;
-        if(n % 4 != 0 || n == 0) return false;
-        return isPowerOfFour( n / 4);
+        // if(n == 1) return true;
+        // if(n % 4 != 0 || n == 0) return false;
+        // return isPowerOfFour( n / 4);
+        //Bit manipulation solution
+        int no_of_ones = 0, pos = 1, one_pos = 0;
+        while(n >= 1){
+            if(n & 1){
+                no_of_ones++;
+                one_pos = pos;
+            }
+            n>>=1;
+            pos++;
+        }
+        return no_of_ones == 1 && (one_pos & 1);
     }
 };
