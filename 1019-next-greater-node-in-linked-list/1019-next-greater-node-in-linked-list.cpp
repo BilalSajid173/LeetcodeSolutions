a238fb5948eaa77e918c7d59aea20df0e271c281
Bilal Sajid <bsajid173@gmail.com>
2022-08-31 19:58:38 +0530

    Time: 93 ms (94.80%), Space: 41.3 MB (71.79%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -10,21 +10,33 @@
  */
 class Solution {
 public:
-    vector<int> nextLargerNodes(ListNode* head) {
-        ListNode* temp = head;
-        vector<int> ans;
-        while(temp){
-            ListNode*temp1 = temp;
-            while(temp1 && temp1->val <= temp->val){
-                temp1 = temp1->next;  
-            }
-            if(temp1){
-                ans.push_back(temp1->val);
-            } else {
-                ans.push_back(0);
-            }
-            temp = temp->next;
-        }
-        return ans;
-    }
+    // vector<int> nextLargerNodes(ListNode* head) {
+    //     ListNode* temp = head;
+    //     vector<int> ans;
+    //     while(temp){
+    //         ListNode*temp1 = temp;
+    //         while(temp1 && temp1->val <= temp->val){
+    //             temp1 = temp1->next;  
+    //         }
+    //         if(temp1){
+    //             ans.push_back(temp1->val);
+    //         } else {
+    //             ans.push_back(0);
+    //         }
+    //         temp = temp->next;
+    //     }
+    //     return ans;
+    // }
+    //stack solution
+    vector<int> nextLargerNodes(ListNode* h) {
+  vector<int> res, stack;
+  for (auto p = h; p != nullptr; p = p->next) res.push_back(p->val);
+  for (int i = res.size() - 1; i >= 0; --i) {
+    auto val = res[i];
+    while (!stack.empty() && stack.back() <= res[i]) stack.pop_back();
+    res[i] = stack.empty() ? 0 : stack.back();
+    stack.push_back(val);
+  }
+  return res;
+}
 };
