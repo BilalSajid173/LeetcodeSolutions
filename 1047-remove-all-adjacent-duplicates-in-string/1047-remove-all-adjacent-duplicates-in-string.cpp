5551b1538934dc2497f754647559666ad98555f0
Bilal Sajid <bsajid173@gmail.com>
2022-09-01 23:46:06 +0530

    Time: 38 ms (51.81%), Space: 11 MB (49.06%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
-        for(auto x:s){
-            if(!st.empty()){
-                if(x == st.top()){
-                    st.pop();
-                } else {
-                    st.push(x);
-                }
-            } else {
-                st.push(x);
-            }
-        }
         string ans = "";
-        while(!st.empty()){
-            ans = st.top() + ans;
-            st.pop();
+        for(auto x:s){
+            if(ans.length() > 0 && ans.back() == x){
+                ans.pop_back();
+            } else {
+                ans.push_back(x);
+            }
         }
         return ans;
     }
