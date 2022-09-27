75879291fc4c6147abfb8c401308fb72e7f25cd5
Bilal Sajid <bsajid173@gmail.com>
2022-09-27 23:31:02 +0530

    Time: 141 ms (13.00%), Space: 34 MB (5.37%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     string pushDominoes(string s) {
-        vector<pair<int,int>> left;
-        vector<pair<int,int>> right;
+        vector<int> left;
+        vector<int> right;
         int rc = 0, lc = 0;
         int rsec = 1, lsec = 1;
         for(auto c:s){
             if(c == '.'){
                 if(rc == 1){
-                    right.push_back({rc, rsec});
+                    right.push_back(rsec);
                     rsec++;
                 } else {
-                    right.push_back({-1,-1});
+                    right.push_back(-1);
                 }
             } else if(c == 'R'){
                 rc = 1;
                 rsec = 1;
-                right.push_back({-1,-1});
+                right.push_back(-1);
             } else if(c == 'L'){
                 rc = 0;
                 rsec = 1;
-                right.push_back({-1,-1});
+                right.push_back(-1);
             }
         }
         for(int i = s.size() - 1;i>=0;i--){
             if(s[i] == '.'){
                 if(lc == 1){
-                    left.push_back({lc, lsec});
+                    left.push_back(lsec);
                     lsec++;
                 } else {
-                    left.push_back({-1,-1});
+                    left.push_back(-1);
                 }
             } else if(s[i] == 'L'){
                 lc = 1;
                 lsec = 1;
-                left.push_back({-1,-1});
+                left.push_back(-1);
             } else if(s[i] == 'R'){
                 lc = 0;
                 lsec = 1;
-                left.push_back({-1,-1});
+                left.push_back(-1);
             }
         }
         reverse(left.begin(), left.end());
         string ans = "";
         for(int i = 0;i<s.size();i++){
-            if(left[i].first == -1 && right[i].first == -1){
+            if(left[i] == -1 && right[i] == -1){
                 ans += s[i];
-            } else if(left[i].first != -1 && right[i].first == -1){
+            } else if(left[i] != -1 && right[i] == -1){
                 ans += 'L';
             }
-            else if(left[i].first == -1 && right[i].first != -1){
+            else if(left[i] == -1 && right[i] != -1){
                 ans += 'R';
-            } else if(left[i].first != -1 && right[i].first != -1){
-                if(left[i].second > right[i].second){
+            } else if(left[i] != -1 && right[i] != -1){
+                if(left[i] > right[i]){
                     ans += 'R';
-                } else if(left[i].second < right[i].second){
+                } else if(left[i] < right[i]){
                     ans += 'L';
                 } else {
                     ans += s[i];
