a67a4e9171388b8aa3c6fcc343e2d4640bdc1c8b
Bilal Sajid <bsajid173@gmail.com>
2022-09-28 18:38:50 +0530

    Time: 14 ms (10.98%), Space: 10.8 MB (32.26%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,23 +10,41 @@
  */
 class Solution {
 public:
-    int Length(struct ListNode* p)
-    {
-        int l=0;
-        while(p)
-        {
-            p=p->next;
-            l++;
-        }
-        return l;
-    }
+    // int Length(struct ListNode* p)
+    // {
+    //     int l=0;
+    //     while(p)
+    //     {
+    //         p=p->next;
+    //         l++;
+    //     }
+    //     return l;
+    // }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-        int diff = Length(head) - n;
-        if(diff == 0) return head->next;
-        struct ListNode *ptr = head;
-        for(int i=0;i<diff-1;i++) ptr=ptr->next;
-        ptr->next=ptr->next->next;
-        return head;
+        // int diff = Length(head) - n;
+        // if(diff == 0) return head->next;
+        // struct ListNode *ptr = head;
+        // for(int i=0;i<diff-1;i++) ptr=ptr->next;
+        // ptr->next=ptr->next->next;
+        // return head;
+        if(head->next == NULL) return head->next;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* fast = head;
+        ListNode *slow = dummy;
+        int k = n;
+        while(k > 0){
+            fast = fast->next;
+            k--;
+        }
+        while(fast!= NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        slow->next = slow->next->next;
+        return dummy->next;
+        
     }
 };
